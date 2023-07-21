f63cfccc2a70a65fd99aa885196ed1ee500c5efb
khan_shazid <shazid1971@gmail.com>
2023-07-21 21:57:01 +0600

    background needed to set for chart element explicitly.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import Paper from '@mui/material/Paper';
+import Chart from 'chart.js/auto'
+import { useEffect, useRef } from 'react';
 
 function App() {
+  // const chartRef = useRef()
+  const labels = ["1", "2", "3", "4", "5"];
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Assessment Score",
+        data: [20, 50, 40, 80, 100],
+        fill: false,
+        borderColor: "#a259ff",
+        tension: 0.1,
+        fill: true,
+      },
+    ],
+  };
+  // const options = {
+  //   scales: {
+  //     x: {
+  //       grid: {
+  //         color: "#3c3c3c",
+  //       },
+  //       ticks: {
+  //         color: "#ffffff",
+  //       },
+  //     },
+  //     y: {
+  //       grid: {
+  //         color: "#3c3c3c",
+  //       },
+  //       ticks: {
+  //         color: "#ffffff",
+  //       },
+  //     },
+  //   },
+  // };
+  const config = {
+    type: 'line',
+    data: data,
+  }
+
+  let myChart = null;
+
+  useEffect(() => {
+    myChart = new Chart(document.getElementById('chart-id'), config);
+    return () => {
+      if (myChart)
+        myChart.destroy()
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        background: "linear-gradient(180deg, #1a1a1a 10%, #4b4b4b 75%)",
+        color: "#1b1b1b",
+        height: "200px",
+        width: "100%",
+      }}
+    >
+      <Paper sx={{ w: 250, h: 400, background: 'red' }}>
+          <p>testing</p>
+          <canvas style={{ background: 'white' }} id="chart-id" />
+      </Paper>
     </div>
+    
   );
-}
+};
 
 export default App;
