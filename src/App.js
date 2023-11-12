8876d8ec3b7177f892cfe8f68304dbf660fc8a6d
khan_shazid <shazid1971@gmail.com>
2023-11-12 13:44:59 +0600

    first and last item fix implemented for react-select.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Select from 'react-select';
+
 
 function App() {
+  const options = [
+    { value: 'first', label: 'first', fixed: true },
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+    { value: 'last', label: 'last', fixed: true },
+  ]
+
+  const customFilter = (option, rawInput) => {
+    const inputValue = rawInput.toLowerCase();
+
+    // If there's no input, include all options
+    if (!inputValue) {
+      return true;
+    }
+
+    // Allow the first and last options to always be visible
+    if (option.data.fixed) {
+      return true;
+    }
+
+    // Apply regular filtering for other options
+    return (
+      option.label.toLowerCase().includes(inputValue) ||
+      option.value.toLowerCase().includes(inputValue)
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Select
+        options={options}
+        isSearchable={true}
+        filterOption={customFilter}
+      />
     </div>
   );
 }
 
 export default App;
+
+
+
+
